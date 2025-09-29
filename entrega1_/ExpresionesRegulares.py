9d6924c40261bc3c7db8322ed5a02857a3338478
Maria Camila Pulido Bedoya <mpulidobe@unal.edu.co>
2025-09-28 21:59:33 -0500

    Update ExpresionesRegulares.py

--- entrega1_/ExpresionesRegulares.py
+++ entrega1_/ExpresionesRegulares.py
@@ -8,3 +8,12 @@ with open ("fundamentos_bio/entrega1_/Escherichia_coli_gca_006351485.ASM635148v1
     for line in file:
         if re.match(r"^>ENSB", line):
             print("Cabecera encontrada:", line.strip())
+
+    '''Funcion re.findall
+    Busca todas las coincidencias dentro de cada línea del archivo
+    '''
+    for line in file:
+        if line.startswith(">"):  # Solo analizamos las cabeceras
+            matches = re.findall(r"(ENSB\w+)", line)  
+            if matches:
+                print("Identificadores encontrados:", matches)
